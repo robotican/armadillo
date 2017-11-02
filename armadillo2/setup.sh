1acb9eb64e095e6ea1abdd630ea33ef69517e622
sub <elhayr1@gmail.com>
2017-11-02 18:02:20 +0200

    start building armadillo sim

--- armadillo2/setup.sh
+++ armadillo2/setup.sh
@@ -1,6 +1,7 @@
 # check that ros kinetic installed
 # install rules to their folder
 # sudo apt-get install ros-kinetic-controller-manager
+# sudo apt-get install ros-kinetic-diff-drive-controller
 # sudo apt-get install ros-kinetic-control-toolbox 
 # sudo apt-get install ros-kinetic-serial
 # sudo apt-get install ros-kinetic-moveit-core
