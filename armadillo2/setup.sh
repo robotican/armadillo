a07bb4f15f3e3d09abc9518aa1d6919630a1e700
sub <elhayr1@gmail.com>
2017-11-02 12:53:53 -0700

    installation file edit

--- armadillo2/setup.sh
+++ armadillo2/setup.sh
@@ -1,8 +1,6 @@
 # check that ros kinetic installed
 # install rules to their folder
-# sudo apt-get install ros-kinetic-controller-manager
-# sudo apt-get install ros-kinetic-diff-drive-controller
-# sudo apt-get install ros-kinetic-control-toolbox 
-# sudo apt-get install ros-kinetic-serial
-# sudo apt-get install ros-kinetic-moveit-core
-# sudo apt-get install ros-kinetic-gripper-action-controller 
+# sudo apt-get install ros-kinetic-controller-manager #
+# sudo apt-get install ros-kinetic-control-toolbox  #
+# sudo apt-get install ros-kinetic-transmission-interface #
+# sudo apt-get install ros-kinetic-joint-limits-interface #
