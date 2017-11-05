4dc87e9062ecca05bd6a38bd27fbcfa9e59988fb
sub <elhayr1@gmail.com>
2017-11-05 13:20:16 -0800

    update

--- armadillo2/setup.sh
+++ armadillo2/setup.sh
@@ -1,6 +1,10 @@
 # check that ros kinetic installed
 # install rules to their folder
+# sudo apt-get update #
 # sudo apt-get install ros-kinetic-controller-manager #
 # sudo apt-get install ros-kinetic-control-toolbox  #
 # sudo apt-get install ros-kinetic-transmission-interface #
 # sudo apt-get install ros-kinetic-joint-limits-interface #
+# sudo apt-get install ros-kinetic-gazebo-ros-control #
+# sudo apt-get install ros-kinetic-ros-controllers #
+# sudo apt-get install ros-kinetic-ros-control #
