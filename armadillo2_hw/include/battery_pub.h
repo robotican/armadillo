c2112c9760de9236320f11597ca8c528adcab3c0
armadillo2 <elhayr1@gmail.com>
2017-11-30 16:28:28 +0200

    getting battery params

--- armadillo2_hw/include/battery_pub.h
+++ armadillo2_hw/include/battery_pub.h
@@ -9,19 +9,25 @@
 #include <bms_interface/bms_interface.h>
 #include <sensor_msgs/BatteryState.h>
 
-#define BATT_PORT "/dev/BMS"
+//#define BATT_PORT "/dev/BMS"
 #define BATT_PUB_INTERVAL 1 //secs
-#define SHOW_WARNINGS false
+#define BATT_PORT_PARAM "batt_port"
 
 namespace armadillo2_hw
 {
     class BatteryPub
     {
     private:
-        ros::NodeHandle *node_handle_;
         ros::Publisher bat_pub_;
         ros::Timer bat_pub_timer_;
         bms::BMSInterface bms_;
+
+        std::string batt_port_;
+        int low_batt_val_ = -1;
+        bool show_warnings_ = false;
+        bool load_battery_hw_ = true;
+
+
 
         void pubTimerCB(const ros::TimerEvent& event);
 
--- armadillo2_hw/src/battery_pub.cpp
+++ armadillo2_hw/src/battery_pub.cpp
@@ -8,9 +8,27 @@ namespace armadillo2_hw
 {
     BatteryPub::BatteryPub(ros::NodeHandle nh)
     {
+        /* get batt params */
+        nh.getParam("load_battery_hw", load_battery_hw_);
+
+        if (!load_battery_hw_)
+            return;
+
+        if (!nh.hasParam(BATT_PORT_PARAM))
+        {
+            ROS_ERROR("[armadillo2_hw/battery_pub]: %s param is missing on param server. make sure that this param exist in battery_config.yaml "
+                              "and that your launch includes this param file. shutting down...", BATT_PORT_PARAM);
+            ros::shutdown();
+            exit (EXIT_FAILURE);
+        }
+        nh.getParam(BATT_PORT_PARAM, batt_port_);
+        nh.getParam("low_batt_val", low_batt_val_);
+        nh.getParam("show_warnings", show_warnings_);
+
+        /* connect to batt FTDI */
         try
         {
-            bms_.connect(BATT_PORT);
+            bms_.connect(batt_port_);
         }
         catch (bms::BMSException exp)
         {
@@ -18,8 +36,9 @@ namespace armadillo2_hw
             ros::shutdown();
             exit(EXIT_FAILURE);
         }
-        ROS_INFO("[armadillo2_hw/battery_pub]: battery port opened successfully \nport name: %s \nbaudrate: 9600", BATT_PORT);
+        ROS_INFO("[armadillo2_hw/battery_pub]: battery port opened successfully \nport name: %s \nbaudrate: 9600", batt_port_.c_str());
 
+        /* batt publisher */
         bat_pub_ = nh.advertise<sensor_msgs::BatteryState>("battery_state", 10);
         bat_pub_timer_ = nh.createTimer(ros::Duration(BATT_PUB_INTERVAL), &BatteryPub::pubTimerCB, this);
         ROS_INFO("[armadillo2_hw/battery_pub]: battery publisher is up");
@@ -43,6 +62,9 @@ namespace armadillo2_hw
             msg.cell_voltage = bms_data.vcells;
             msg.location = "base_link";
 
+            if (low_batt_val_ >=0 && msg.percentage <= low_batt_val_)
+                ROS_WARN("[armadillo2_hw/battery_pub]: LOW BATTERY, please connect Armadillo2 to charger");
+
             bat_pub_.publish(msg);
         }
         catch(bms::BMSErrorException exp)
@@ -53,7 +75,7 @@ namespace armadillo2_hw
         }
         catch(bms::BMSWarnException exp)
         {
-            if (SHOW_WARNINGS)
+            if (show_warnings_)
                 ROS_WARN("[armadillo2_hw/battery_pub]: %s", exp.what());
         }
 
@@ -63,11 +85,15 @@ namespace armadillo2_hw
 
     void BatteryPub::startPublish()
     {
+        if (!load_battery_hw_)
+            return;
         bat_pub_timer_.start();
     }
 
     void BatteryPub::stopPublish()
     {
+        if (!load_battery_hw_)
+            return;
         bat_pub_timer_.stop();
     }
 }
